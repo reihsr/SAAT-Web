3f95dafe9bafe74d486a1ba2c1812d42969cc673
Robert Reihs <robert.reihs@medunigraz.at>
2020-04-11 10:20:07 +0200

    Frontend update tree node component

--- ui/src/main/webui/src/app/components/dictionary-tree-node/dictionary-tree-node.component.ts
+++ ui/src/main/webui/src/app/components/dictionary-tree-node/dictionary-tree-node.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DictionaryTreeNode } from '../../model/dictionary-tree-node';
 
 @Component({
   selector: 'app-dictionary-tree-node',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DictionaryTreeNodeComponent implements OnInit {
 
-  constructor() { }
+  public dictionaryTreeNode$: DictionaryTreeNode;
+
+  constructor() {
+    this.dictionaryTreeNode$ = {
+      dictionary_id: 1,
+      synonym: 'Synonym',
+      before_synonym: 0,
+      after_synonmy: 0,
+      foreword: false,
+      ending: false,
+      sentence: false,
+      occure: false,
+      root: false,
+      iscode: false,
+      negation: false,
+      pattern: 'pattern',
+      code_typ: 'code_typ',
+      code_value: 'code_value',
+      priority_node: false,
+      priority: 1
+    };
+  }
 
   ngOnInit(): void {
 
